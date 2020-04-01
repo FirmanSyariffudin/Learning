52cfef3408ed534ab6b370cfadd3a4c4ab3a746d
Firman Syariffudin <firmansmkts@gmail.com>
2020-04-02 01:10:57 +0700

    1

--- javascript/4.OPERATOR/Operator.js
+++ javascript/4.OPERATOR/Operator.js
@@ -4,4 +4,12 @@
 "Binary= Sebuah operator membutuhkan 2 operand = operand1 operator operand2 (1+1)"
 "Ternary= Sebuah operator membutuhkan 3 operand"
 "Urutan Operasi Matematika (KuKaBaTaKu= () * / + - ) kurung kali bagi tambah kurang"
-"Penugasan seperti x = 10 (menugaskan operand x kedalam operand 10)"
+"Penugasan seperti x = 10 (menugaskan operand x kedalam operand 10)", "Beberapa macam operator penugasan= (+=) (/=) dll"
+"Perbandingan seperti= (==) (!=) (===) dll mereka akan menghasilkan nilai boolean(TRUE / FALSE)"
+
+
+"Operator Logika = AND OR NOT (&& || !)", "kalau && keduanya harus TRUE, kalau || asal salah satu benar hasilnya akan TRUE, kalau ! adalah kebalikan hasil TRUE akan menjadi FALSE"
+
+"Operator Typeof= untuk mengetahui tipedata apa di dalam operandnya", "Misal typeof(10) hasilnya number", "Misal typeof(true) hasilnya boolean"
+
+"Operator Kondisional= Pengecekan kondisi dan menentukan nilai ketika kondisi bernilai TRUE atau FALSE", "(Operand) ? TRUE : FALSE", ' Misal (x % 2 == 0) ? "genap" : "ganjil" ' 
---
+++ javascript/5.STRING/string.js
@@ -0,0 +1,5 @@
+" Jika menggunakan string dengan kutip dua maka pembungkusnya harus kutip satu , dan sebaliknya"
+//inicomentbiargakeror , misal ada case " "PIKET" dilaksanakan pada hari Jum'at " //maka akan eror
+"Jika memang membutuhkan keduanya maka kita butuh escape character = yaitu tanda backslash \ ", "Macam escape char= \n untuk newline(baris baru), \t untuk tab,  dll"
+'ini solusi case yg benar',  " \"PIKET\" dilaksanakan pada hari Jum'at " 
+"Backslash akan merepresentasikan kutip menjadi berbeda (yaitu String Kutip) dan bukan sebagai pembungkus"
