408eb217d1c34eeb0670a2e9ef3c11a0314dbb70
Firman Syariffudin <firmansmkts@gmail.com>
2020-04-02 01:07:04 +0700

    Add files via upload

---
+++ javascript/4.OPERATOR/operator.js
@@ -0,0 +1,15 @@
+"Operator ialah symbol yang akan melakukan operasi"
+"Macam Operator = Aritmatika, Penugasan, Perbandingan, Logika, String,(Ialah Operator Binary), Kondisional (Ialah Operator Ternary), Typeof (Ialah Operator Unary)"
+"Unary= Sebuah operator membutuhkan 1 operand = operand operator atau kebalikannya (1+ atau +1)"
+"Binary= Sebuah operator membutuhkan 2 operand = operand1 operator operand2 (1+1)"
+"Ternary= Sebuah operator membutuhkan 3 operand"
+"Urutan Operasi Matematika (KuKaBaTaKu= () * / + - ) kurung kali bagi tambah kurang"
+"Penugasan seperti x = 10 (menugaskan operand x kedalam operand 10)", "Beberapa macam operator penugasan= (+=) (/=) dll"
+"Perbandingan seperti= (==) (!=) (===) dll mereka akan menghasilkan nilai boolean(TRUE / FALSE)"
+
+
+"Operator Logika = AND OR NOT (&& || !)", "kalau && keduanya harus TRUE, kalau || asal salah satu benar hasilnya akan TRUE, kalau ! adalah kebalikan hasil TRUE akan menjadi FALSE"
+
+"Operator Typeof= untuk mengetahui tipedata apa di dalam operandnya", "Misal typeof(10) hasilnya number", "Misal typeof(true) hasilnya boolean"
+
+"Operator Kondisional= Pengecekan kondisi dan menentukan nilai ketika kondisi bernilai TRUE atau FALSE", "(Operand) ? TRUE : FALSE", ' Misal (x % 2 == 0) ? "genap" : "ganjil" ' 
